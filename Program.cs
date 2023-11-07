1b07577e62ba06e9ce46d9f6980abf59237fd5fe
sykes <sykes@LAPTOP-1HUNKKOC>
2023-11-07 16:37:14 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Blocks_World_Strips_Planner
+{
+    class Blocks_World_Strips_Planner
+    {
+        static List<String> goalState = new List<String>();
+        static List<String> startState;
+
+        static FileStream fileStart, fileGoal;
+        static string pathStart, pathGoal;
+
+        static void Main(string[] args)
+        {
+            CreateData();
+            //PrintStates();
+            
+            Console.ReadKey();
+        }
+        static void CreateData()
+        {
+            pathStart = @"C:\Users\sykes\OneDrive\Desktop\CodingProjects\Blocks World Strips Planner\StartState.txt";
+            pathGoal = @"C:\Users\sykes\OneDrive\Desktop\CodingProjects\Blocks World Strips Planner\GoalState.txt";
+
+            startState = File.ReadAllLines(pathStart).ToList();
+            goalState = File.ReadAllLines(pathGoal).ToList();
+
+            
+        }
+
+        static void PrintStates()
+        {
+            Console.WriteLine("Start State");
+            foreach (string line in startState)
+            {
+                Console.WriteLine(line);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Goal State");
+            foreach (string line in goalState)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ Word.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blocks_World_Strips_Planner
+{
+    class Word
+    {
+        enum WordType
+        {
+            Action,
+            Predicate
+        }
+
+        string 
+    }
+}
